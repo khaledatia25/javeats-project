1ea43afeee2b0a8ab05dfe2cec043b573fde21e4
khaled atia <khalodatia@gmail.com>
2023-12-03 22:53:50 +0200

    add readme

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,9 @@ import cartItemRouter from "@routes/cartItem.route";
 import ordersRouter from "@routes/orders.route";
 import restaurantRouter from "@routes/restaurant.route";
 import dotenv from "dotenv";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerJsdoc from "swagger-jsdoc";
+import * as path from "path";
 
 dotenv.config();
 
@@ -20,6 +23,18 @@ app.use(cors());
 app.get("/v1", (_req: Request, res: Response) => {
   res.send("khaled waleed");
 });
+
+// Swagger options
+const swaggerOptions: swaggerJsdoc.Options = {
+  swaggerDefinition: require(path.join(__dirname, "swagger.json")),
+  apis: [path.join(__dirname, "./routes/*.ts")],
+};
+
+// Initialize swagger-jsdoc
+const swaggerSpec = swaggerJsdoc(swaggerOptions);
+
+// Serve Swagger UI at /api-docs
+app.use("/v1/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/v1", customerRouter);
 app.use("/v1", adminRouter);
